0aa3d3ce26e88b93cb23d5e8465ddffca4755b6c
JonathanRolon <rolon.jonathan.ezequiel@gmail.com>
2019-11-24 12:28:11 -0300

    selenium tests solo de local(razon:no encuentra el webdriver)

--- src/test/java/hellocucumber/selenium/ConsultarVersionesProductosCliente.java
+++ src/test/java/hellocucumber/selenium/ConsultarVersionesProductosCliente.java
@@ -13,13 +13,39 @@ public class ConsultarVersionesProductosCliente {
  	
 	String cliente;
 	
+	public ConsultarVersionesProductosCliente(){
+		driver  = new ChromeDriver();
+    	System.setProperty("webdriver.chrome.driver", "/home/jonathan/Desktop/cucumber/chromedriver");					
+        driver.manage().window().maximize();
+	}
+	
+	@Given("estoy parado sobre la seccion home")
+	public void estoy_parado_sobre_la_seccion_home() {
+		driver.get("https://selenium-tests.000webhostapp.com/pantalla_principal.html");		
+	}
+	
+	@When("selecciono Clientes")
+	public void selecciono_clientes() {
+		//agrego un retraso leve de 3 segundos para mostrar bien la accion
+		try{
+		   Thread.sleep(3000);
+    	}catch(InterruptedException e){}
+		driver.findElement(By.name("clientes_button")).click();
+	}
+	
+	@Then("el sistema me lista los clientes asociados a la empresa")
+	public void el_sistema_me_lista_los_clientes_asociados_a_la_empresa() {
+
+		//agrego un retraso leve de 3 segundos para mostrar bien la accion
+		try{
+		   Thread.sleep(3000);
+    	}catch(InterruptedException e){}
+	}
+	
     @Given("estoy en la seccion de clientes")					
     public void estoy_en_la_seccion_de_clientes() throws Throwable							
     {
-    	driver  = new ChromeDriver();
-    	System.setProperty("webdriver.chrome.driver", "/home/jonathan/Desktop/cucumber/chromedriver");					
-        driver.manage().window().maximize();
-        driver.get("https://selenium-tests.000webhostapp.com/ClienteTestPepsiCo.html");		
+    	driver.get("https://selenium-tests.000webhostapp.com/ClienteTestPepsiCo.html");
     }		
 
     @When("selecciono {string}")			
@@ -31,6 +57,7 @@ public class ConsultarVersionesProductosCliente {
     @Then("los productos asociados al cliente se listan")					
     public void	los_productos_asociados_al_cliente_se_listan() throws Throwable 							
     {			
+
     	driver.findElement(By.name(cliente)).click();
     	try{
 		   Thread.sleep(3000);
