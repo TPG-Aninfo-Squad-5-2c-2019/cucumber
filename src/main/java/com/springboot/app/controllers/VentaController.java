77f7f49352358b27a810bc21ca097c10dcaa8f6e
JonathanRolon <rolon.jonathan.ezequiel@gmail.com>
2019-11-28 11:05:01 -0300

    agregado lo de ventas

--- src/main/java/com/springboot/app/controllers/VentaController.java
+++ src/main/java/com/springboot/app/controllers/VentaController.java
@@ -2,17 +2,20 @@ package com.springboot.app.controllers;
 
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.springboot.app.models.dao.VentaDao;
 import com.springboot.app.models.entity.Venta;
 
+@Controller
 public class VentaController {
 	
 	private VentaDao vdao =  VentaDao.getInstance();
 	
-	@RequestMapping(value="/sales")
+	@RequestMapping(value="/sales", method= RequestMethod.GET)
 	public String listarVentas(Model m) {
 		List<Venta> ventas = vdao.getAll();
 		
